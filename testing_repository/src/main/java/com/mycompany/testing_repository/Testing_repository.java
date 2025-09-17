788cd81ddc50dd8dc42b3aa2a623a1a12eafd7d0
BayronPonce03 <bponce70921@ufide.ac.cr>
2025-09-16 19:42:27 -0600

    first commit

---
+++ testing_repository/src/main/java/com/mycompany/testing_repository/Testing_repository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testing_repository;
+
+/**
+ *
+ * @author bayro
+ */
+public class Testing_repository {
+
+    public static void main(String[] args) {
+        int numero = 7;
+        
+        System.out.println("Soy un numero: "+ numero);
+    }
+ }
